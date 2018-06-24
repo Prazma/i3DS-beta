a402061ff376f8b49030e6818649c7b15fd0f334
Tomoyoshi Yamamoto <tomoyoshi.usa@gmail.com>
2018-06-24 23:27:53 +0900

    change ui style for better ux

--- js/3dsUI.js
+++ js/3dsUI.js
@@ -2,15 +2,15 @@
 var topScreen = document.getElementById("topScreen");
 var bottomScreen = document.getElementById("bottomScreen");
 window.onload = function () {
-    topScreen.style.height = window.innerHeight * 0.3 + "px";
+    topScreen.style.height = window.innerHeight * 0.48 + "px";
     topScreen.style.width = window.innerWidth * 0.9 + "px";
-    bottomScreen.style.height = window.innerHeight * 0.3 + "px";
+    bottomScreen.style.height = window.innerHeight * 0.48 + "px";
     bottomScreen.style.width = window.innerWidth * 0.9 + "px";
 }
 window.onresize = function () {
-    topScreen.style.height = window.innerHeight * 0.3 + "px";
+    topScreen.style.height = window.innerHeight * 0.48 + "px";
     topScreen.style.width = window.innerWidth * 0.9 + "px";
-    bottomScreen.style.height = window.innerHeight * 0.3 + "px";
+    bottomScreen.style.height = window.innerHeight * 0.48 + "px";
     bottomScreen.style.width = window.innerWidth * 0.9 + "px";
 }
 //below are the scripts to handle user inputs like touch and button press
